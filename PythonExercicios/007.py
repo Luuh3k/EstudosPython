21811e12c3d75360ad488a45a21582536d2190b9
luuh3k <lucas.digital18@gmail.com>
2025-11-17 08:27:30 -0300

    Atualização do exercício 7

--- PythonExercicios/007.py
+++ PythonExercicios/007.py
@@ -2,3 +2,5 @@ larg = float (input('largua da parede: '))
 alt = float(input('altura da parede: '))
 área = larg * alt
 print ('sua parede tem a dimensão de {}x{} e sua área é de {}m².'.format(larg,alt, área))
+tinta = área / 2
+print('Para pintar essa parede, você vai precisar de {}l de tinta.'.format(tinta))
